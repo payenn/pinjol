1c663a6b81bc2dae37dcfd613030a05e453d7181
payenn <calistavalencia13@gmail.com>
2025-06-18 13:21:18 +0700

    Add files via upload

---
+++ model/Admin.java
@@ -0,0 +1,37 @@
+
+
+package latihan.model;
+
+
+public class Admin {
+
+    private int id;
+    private String nama;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
---
+++ model/Nasabah.java
@@ -0,0 +1,44 @@
+
+package latihan.model;
+
+
+public class Nasabah {
+    private int Id;
+    private String Nama;
+    private String Hp;
+    private double jumlah;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+    
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String Nama) {
+        this.Nama = Nama;
+    }
+
+    public String getHp() {
+        return Hp;
+    }
+
+    public void setHp(String Hp) {
+        this.Hp = Hp;
+    }
+
+    public double getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(double jumlah) {
+        this.jumlah = jumlah;
+    }
+    
+    
+}
